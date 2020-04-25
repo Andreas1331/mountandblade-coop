b83e922980e0afcbab706c51c2eba35cc4c16df1
-Andreas <andreaschristensen2010@live.dk>
2020-04-25 10:04:49 +0200

    Client project setup

---
+++ MBCoopMod/MBCoopClient/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.InputSystem;
+using TaleWorlds.MountAndBlade;
+
+namespace MBCoopClient
+{
+    public class Main : MBSubModuleBase
+    {
+        protected override void OnApplicationTick(float dt)
+        {
+            if (Input.IsKeyPressed(InputKey.K))
+            {
+                InformationManager.DisplayMessage(new InformationMessage("Hey from Coop!"));
+            }
+        }
+    }
+}

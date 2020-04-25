4bb08a5f40c5a3fd39089e6b37eec4a5edb184ba
-Andreas <andreaschristensen2010@live.dk>
2020-04-25 10:09:09 +0200

    Added coop server + Multiple project startup on F5

---
+++ MBCoopMod/MBCoopServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBCoopServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server started!");
+            Console.ReadKey();
+        }
+    }
+}
